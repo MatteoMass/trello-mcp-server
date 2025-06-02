edfaef070ffdd56850ffa0cae4e5094fd7f4b2d0
MatteoMass <matteomassetti8@gmail.com>
2025-06-02 11:04:40 +0200

    [ADD] tools

--- server/services/board.py
+++ server/services/board.py
@@ -28,7 +28,7 @@ class BoardService:
         response = await self.client.GET(f"/boards/{board_id}")
         return TrelloBoard(**response)
 
-    async def get_boards(self, member_id: str = "me") -> List[TrelloBoard]:
+    async def get_boards(self, member_id: str = "me") -> dict[str, List[TrelloBoard]]:
         """Retrieves all boards for a given member.
 
         Args:
@@ -38,7 +38,21 @@ class BoardService:
             List[TrelloBoard]: A list of board objects.
         """
         response = await self.client.GET(f"/members/{member_id}/boards")
-        return [TrelloBoard(**board) for board in response]
+        return {"boards": [TrelloBoard(**board) for board in response]}
+    
+
+    async def get_boards_names(self, member_id: str = "me") -> dict[str, List[str]]:
+        """Retrieves all boards for a given member.
+
+        Args:
+            member_id (str): The ID of the member whose boards to retrieve. Defaults to "me" for the authenticated user.
+
+        Returns:
+            List[TrelloBoard]: A list of board objects.
+        """
+        response = await self.client.GET(f"/members/{member_id}/boards")
+        boards = [TrelloBoard(**board) for board in response]
+        return {"boards_names": [board.name for board in boards]}   
 
     async def get_board_labels(self, board_id: str) -> List[TrelloLabel]:
         """Retrieves all labels for a specific board.
